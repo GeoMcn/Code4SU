3ee0fbd95e16b27bee00683d53b5bcce97e3efa1
George McNally <geomcn11@gmail.com>
2019-03-13 15:24:32 +0000

    added jquery toaster notifications

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'materialize-sass'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.6', '>= 4.6.1'
+gem 'toastr_rails'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+#module ApplicationHelper
+#    def avatar_url(user)
+#        gravatar_url.id = Digest::MD5::hexdigest(user.email).downcase "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=40"
+#    end
+#end
+
 module ApplicationHelper
+def avatar_url(user, size)
+gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+gravatar_url= "https://secure.gravatar.com/avatar/#{gravatar_id}.png?d=identicon&s=#{size}"
+image_tag(gravatar_url, alt: user.name, class: "circle responsive_img")
+end
 end
